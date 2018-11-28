0e35dd44ededc7226f8daca01ffcb8ce00748192
Joshua <joshuadstarkey@gmail.com>
2018-11-28 00:36:49 -0800

    Gotta start somewhere

---
+++ lira.js
@@ -0,0 +1,5 @@
+// OMDb API Key: http://www.omdbapi.com/?i=tt3896198&apikey=558181ec
+
+// Spotify API Key: 
+// Client ID d6f095ab25264f24b164e078df3b680b
+// Client secret can be found here: https://developer.spotify.com/dashboard/applications/d6f095ab25264f24b164e078df3b680b
