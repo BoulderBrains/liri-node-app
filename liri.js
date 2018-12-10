2415a7f1ec1e0a9dfeef3e1af82262728a9b4526
Joshua <joshuadstarkey@gmail.com>
2018-12-10 12:09:42 -0800

    refinements

--- liri.js
+++ liri.js
@@ -26,21 +26,80 @@ switch (action) {
 		doWhatItSays()
 }
 
-function movieSearch() {
+function bandSearch() {
 	for (var i = 3; i < allArgs.length; i++) {
-		searchResult += (allArgs[i] + "+")
+		searchResult += allArgs[i]
+		artist += (allArgs[i] + " ")
 	}
 
-	searchResult = searchResult.slice(0, -1)
+	artist = artist.slice(0, -1)
 
-
-	var queryUrl = "http://www.omdbapi.com/?t=" + searchResult + "&y=&plot=short&apikey=trilogy";
-	var nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+	var queryUrl = "https://rest.bandsintown.com/artists/" + searchResult + "/events?app_id=codingbootcamp"
 
 	if (searchResult === "") {
-		omdbResults(nobodyUrl)
+		console.log("Please put in a band/artist")
 	} else {
-		omdbResults(queryUrl);
+		axios.get(queryUrl).then(
+			function (response) {
+				data = response.data
+				dataLength = response.data.length
+
+				if (dataLength > 0) {
+					for (var j = 0; j < dataLength; j++) {
+
+						console.log("======================")
+						console.log("The venue is at " + data[j].venue.name);
+
+						if (data[j].venue.region === "") {
+							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.country);
+							venue = data[j].venue.city + ", " + data[j].venue.country
+						} else {
+							venue = data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country
+							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country);
+						}
+
+						date = new Date(data[j].datetime);
+
+						var momentObj = moment(date);
+						var momentString = momentObj.format('MM/DD/YYYY');
+
+						console.log("The date is: ", momentString);
+
+						showData = [
+							"The venue is at " + data[j].venue.name,
+							"The location is at: " + venue,
+							"The date is: " + momentString
+						].join("\n\n");
+
+						fs.appendFile("log.txt", showData + "\n==========================\n", function (err) {
+							if (err) {
+								return console.log(err);
+							}
+						});
+					}
+					console.log("log.txt was updated!");
+				} else {
+					console.log("Uh oh, there doesn't look like there's any upcoming concerts from " + artist)
+				}
+			}).catch(function (error) {
+				if (error.response) {
+					console.log("error alert")
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.log(error.response.data);
+					console.log(error.response.status);
+					console.log(error.response.headers);
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an object that comes back with details pertaining to the error that occurred.
+					console.log(error.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.log("Error", error.message);
+				}
+				console.log(error.config);
+			}
+			);
 	}
 }
 
@@ -49,20 +108,20 @@ function omdbResults(url) {
 		function (response) {
 			data = response.data
 			if (data.Response === "True") {
-				console.log("Release Year: ", data.Year);
-				console.log("The Title: ", data.Title)
-				console.log("Imdb Rating: ", data.imdbRating);
-				console.log("The Country: ", data.Country)
+				console.log("Release year: ", data.Year);
+				console.log("Movie title: ", data.Title)
+				console.log("Imdb rating: ", data.imdbRating);
+				console.log("Country released: ", data.Country)
 				if (data.Ratings.length > 1) {
-					console.log("Rotten Tomatoes Rating: ", data.Ratings[1].Value);
+					console.log("Rotten tomatoes rating: ", data.Ratings[1].Value);
 					var rottenRating = data.Ratings[1].Value
 				} else {
 					console.log("Only IMDB Rating is available: ", data.Ratings[0].Value);
 					rottenRating = data.Ratings[0].Value
 				}
-				console.log("The Language: ", data.Language);
-				console.log("The Plot: ", data.Plot);
-				console.log("The Actors: ", data.Actors);
+				console.log("Language: ", data.Language);
+				console.log("Plot: ", data.Plot);
+				console.log("Actors: ", data.Actors);
 			} else {
 				console.log(data.Error)
 			}
@@ -89,8 +148,6 @@ function omdbResults(url) {
 };
 
 function spotifySearch() {
-
-
 	for (var i = 3; i < allArgs.length; i++) {
 		searchResult += (allArgs[i] + " ")
 	}
@@ -171,80 +228,21 @@ function spotifySearch() {
 	}
 };
 
-function bandSearch() {
+function movieSearch() {
 	for (var i = 3; i < allArgs.length; i++) {
-		searchResult += allArgs[i]
-		artist += (allArgs[i] + " ")
+		searchResult += (allArgs[i] + "+")
 	}
 
-	artist = artist.slice(0, -1)
+	searchResult = searchResult.slice(0, -1)
 
-	var queryUrl = "https://rest.bandsintown.com/artists/" + searchResult + "/events?app_id=codingbootcamp"
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + searchResult + "&y=&plot=short&apikey=trilogy";
+	var nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
 
 	if (searchResult === "") {
-		console.log("Please put in a band/artist")
+		omdbResults(nobodyUrl)
 	} else {
-		axios.get(queryUrl).then(
-			function (response) {
-				data = response.data
-				dataLength = response.data.length
-
-				if (dataLength > 0) {
-					for (var j = 0; j < dataLength; j++) {
-
-						console.log("======================")
-						console.log("The venue is at " + data[j].venue.name);
-
-						if (data[j].venue.region === "") {
-							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.country);
-							venue = data[j].venue.city + ", " + data[j].venue.country
-						} else {
-							venue = data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country
-							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country);
-						}
-
-						date = new Date(data[j].datetime);
-
-						var momentObj = moment(date);
-						var momentString = momentObj.format('MM/DD/YYYY');
-
-						console.log("The date is: ", momentString);
-
-						showData = [
-							"The venue is at " + data[j].venue.name,
-							"The location is at: " + venue,
-							"The date is: " + momentString
-						].join("\n\n");
-
-						fs.appendFile("log.txt", showData + "\n==========================\n", function (err) {
-							if (err) {
-								return console.log(err);
-							}
-						});
-					}
-					console.log("log.txt was updated!");
-				} else {
-					console.log("Uh oh, there doesn't look like there's any upcoming concerts from " + artist)
-				}
-			}).catch(function (error) {
-				if (error.response) {
-					console.log("error alert")
-					// The request was made and the server responded with a status code
-					// that falls out of the range of 2xx
-					console.log(error.response.data);
-					console.log(error.response.status);
-					console.log(error.response.headers);
-				} else if (error.request) {
-					// The request was made but no response was received
-					// `error.request` is an object that comes back with details pertaining to the error that occurred.
-					console.log(error.request);
-				} else {
-					// Something happened in setting up the request that triggered an Error
-					console.log("Error", error.message);
-				}
-				console.log(error.config);
-			}
-			);
+		omdbResults(queryUrl);
 	}
 }
 
