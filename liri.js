8e3136787ed5e908cf464d1f555c6fc0aa75ad3e
Joshua <joshuadstarkey@gmail.com>
2018-12-11 08:55:25 -0800

    lots of cleanup

--- liri.js
+++ liri.js
@@ -5,57 +5,58 @@ var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
-var moment = require("moment")
-var action = process.argv[2]
-var allArgs = process.argv
+var moment = require("moment");
+var action = process.argv[2];
+var allArgs = process.argv;
 var searchResult = "";
 var artist = "";
 var showData = [];
 
 switch (action) {
 	case "movie-this":
-		movieSearch()
+		movieSearch();
 		break;
 	case "concert-this":
-		bandSearch()
+		bandSearch();
 		break;
 	case "spotify-this-song":
 		spotifySearch();
 		break;
 	case "do-what-it-says":
-		doWhatItSays()
+		doWhatItSays();
+		break;
 }
 
 function bandSearch() {
 	for (var i = 3; i < allArgs.length; i++) {
 		searchResult += allArgs[i]
-		artist += (allArgs[i] + " ")
+		artist += (allArgs[i] + " ");
 	}
 
-	artist = artist.slice(0, -1)
+	artist = artist.slice(0, -1);
 
-	var queryUrl = "https://rest.bandsintown.com/artists/" + searchResult + "/events?app_id=codingbootcamp"
+	var queryUrl = "https://rest.bandsintown.com/artists/" + searchResult + "/events?app_id=codingbootcamp";
 
 	if (searchResult === "") {
-		console.log("Please put in a band/artist")
+		console.log("Please enter a band or artist");
 	} else {
 		axios.get(queryUrl).then(
 			function (response) {
-				data = response.data
-				dataLength = response.data.length
+				data = response.data;
+				dataLength = response.data.length;
 
 				if (dataLength > 0) {
 					for (var j = 0; j < dataLength; j++) {
 
-						console.log("================")
-						console.log("The venue is at " + data[j].venue.name);
+						console.log("================");
+						console.log("Venue name: " + data[j].venue.name);
 
 						if (data[j].venue.region === "") {
-							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.country);
+							console.log("Location of show:  " + data[j].venue.city + ", " + data[j].venue.country);
 							venue = data[j].venue.city + ", " + data[j].venue.country
 						} else {
-							venue = data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country
-							console.log("The location is at:  " + data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country);
+							venue = data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country;
+							console.log("Location of show:  " + data[j].venue.city + ", " + data[j].venue.region + ", " + data[j].venue.country);
 						}
 
 						date = new Date(data[j].datetime);
@@ -63,12 +64,12 @@ function bandSearch() {
 						var momentObj = moment(date);
 						var momentString = momentObj.format('MM/DD/YYYY');
 
-						console.log("The date is: ", momentString);
+						console.log("Date of show: ", momentString);
 
 						showData = [
-							"The venue is at " + data[j].venue.name,
-							"The location is at: " + venue,
-							"The date is: " + momentString
+							"Venue name: " + data[j].venue.name,
+							"Location of show: " + venue,
+							"Date of show: " + momentString
 						].join("\n\n");
 
 						fs.appendFile("log.txt", showData + "\n==========================\n", function (err) {
@@ -79,22 +80,20 @@ function bandSearch() {
 					}
 					console.log("log.txt was updated!");
 				} else {
-					console.log("Uh oh, there doesn't look like there's any upcoming concerts from " + artist)
+					console.log(artist + "doesn't have any upcoming shows. Go buy there album.");
 				}
 			}).catch(function (error) {
 				if (error.response) {
-					console.log("error alert")
-					// The request was made and the server responded with a status code
-					// that falls out of the range of 2xx
+					console.log("error alert");
+					// The request was made but an error was returned					
 					console.log(error.response.data);
 					console.log(error.response.status);
 					console.log(error.response.headers);
 				} else if (error.request) {
-					// The request was made but no response was received
-					// `error.request` is an object that comes back with details pertaining to the error that occurred.
+					// The request was made but no response was returned
 					console.log(error.request);
 				} else {
-					// Something happened in setting up the request that triggered an Error
+					// Something in the request setup failed
 					console.log("Error", error.message);
 				}
 				console.log(error.config);
@@ -109,32 +108,32 @@ function omdbResults(url) {
 			data = response.data
 			if (data.Response === "True") {
 				console.log("Release year: ", data.Year);
-				console.log("Movie title: ", data.Title)
+				console.log("Movie title: ", data.Title);
 				console.log("Imdb rating: ", data.imdbRating);
-				console.log("Country released: ", data.Country)
+				console.log("Country released: ", data.Country);
 				if (data.Ratings.length > 1) {
 					console.log("Rotten tomatoes rating: ", data.Ratings[1].Value);
-					var rottenRating = data.Ratings[1].Value
+					var rottenRating = data.Ratings[1].Value;
 				} else {
 					console.log("Only IMDB Rating is available: ", data.Ratings[0].Value);
-					rottenRating = data.Ratings[0].Value
+					rottenRating = data.Ratings[0].Value;
 				}
 				console.log("Language: ", data.Language);
 				console.log("Plot: ", data.Plot);
 				console.log("Actors: ", data.Actors);
 			} else {
-				console.log(data.Error)
+				console.log(data.Error);
 			}
 
 			showData = [
-				"Release Year: " + data.Year,
-				"The Title: " + data.Title,
-				"Imdb Rating: " + data.imdbRating,
-				"Rotten Tomatoes Rating: " + rottenRating,
-				"The Country: " + data.Country,
-				"The Language: " + data.Language,
-				"The Plot: " + data.Plot,
-				"The Actors: " + data.Actors
+				"Release year: " + data.Year,
+				"Movie title: " + data.Title,
+				"Imdb rating: " + data.imdbRating,
+				"Rotten Tomatoes rating: " + rottenRating,
+				"Country released: " + data.Country,
+				"Language: " + data.Language,
+				"Plot: " + data.Plot,
+				"Actors: " + data.Actors
 			].join("\n\n");
 
 			fs.appendFile("log.txt", showData + "\n==========================\n", function (err) {
@@ -149,10 +148,10 @@ function omdbResults(url) {
 
 function spotifySearch() {
 	for (var i = 3; i < allArgs.length; i++) {
-		searchResult += (allArgs[i] + " ")
+		searchResult += (allArgs[i] + " ");
 	}
 
-	searchResult = searchResult.slice(0, -1)
+	searchResult = searchResult.slice(0, -1);
 
 	if (searchResult === "") {
 		spotify.search({ type: 'track', query: 'The Sign' }, function (err, data) {
@@ -160,20 +159,15 @@ function spotifySearch() {
 				return console.log('Error occurred: ' + err);
 			}
 
-			console.log("The Artist's name is", data.tracks.items[9].album.artists[0].name)
-			console.log("The Album is", data.tracks.items[9].album.name)
-			console.log("The song name is", data.tracks.items[9].name)
-			console.log("Listen a preview: ", data.tracks.items[9].preview_url)
-			// for (var j = 0; j < data.tracks.items.length; j++) {
-			// 		console.log("======================")
-			// 		console.log(j)
-			// 		console.log("The Artist's name is",data.tracks.items[j].album.artists[0].name)
-			// }
+			console.log("By the artist:", data.tracks.items[9].album.artists[0].name);
+			console.log("Off the album:", data.tracks.items[9].album.name);
+			console.log("Song title:", data.tracks.items[9].name);
+			console.log("Listen a preview: ", data.tracks.items[9].preview_url);
 
 			showData = [
-				"The Artist's name is: " + data.tracks.items[9].album.artists[0].name,
-				"The Album is: " + data.tracks.items[9].album.name,
-				"The song name is: " + data.tracks.items[9].name,
+				"By the artist:: " + data.tracks.items[9].album.artists[0].name,
+				"Off the album: " + data.tracks.items[9].album.name,
+				"Song title: " + data.tracks.items[9].name,
 				"Listen a preview: " + data.tracks.items[9].preview_url
 			].join("\n\n");
 
@@ -190,27 +184,27 @@ function spotifySearch() {
 				return console.log('Error occurred:  ' + err);
 			}
 
-			var songData = data.tracks.items
-			var songLength = data.tracks.items.length
+			var songData = data.tracks.items;
+			var songLength = data.tracks.items.length;
 
 			if (songLength > 0) {
 				for (var j = 0; j < songLength; j++) {
-					console.log("======================")
-					console.log("The song name is", songData[j].name)
-					console.log("The Artist's name is", songData[j].album.artists[0].name)
-					console.log("The Album is", songData[j].album.name)
+					console.log("=====================");
+					console.log("Song title:", songData[j].name);
+					console.log("By the artist:", songData[j].album.artists[0].name);
+					console.log("Off the album:", songData[j].album.name);
 					if (songData[j].preview_url === null) {
-						console.log("There's no preview but you can listen to it on Spotify: ", songData[j].album.external_urls.spotify)
-						var preview = songData[j].album.external_urls.spotify
+						console.log("There's no preview, but you can listen to it on Spotify: ", songData[j].album.external_urls.spotify);
+						var preview = songData[j].album.external_urls.spotify;
 					} else {
-						console.log("Listen a preview: ", songData[j].preview_url)
-						preview = songData[j].preview_url
+						console.log("Hear a preview: ", songData[j].preview_url);
+						preview = songData[j].preview_url;
 					}
 
 					showData = [
-						"The Artist's name is: " + data.tracks.items[j].album.artists[0].name,
-						"The Album is: " + data.tracks.items[j].album.name,
-						"The song name is: " + data.tracks.items[j].name,
+						"By the artist: " + data.tracks.items[j].album.artists[0].name,
+						"Off the album: " + data.tracks.items[j].album.name,
+						"Song title:: " + data.tracks.items[j].name,
 						"Listen a preview: " + preview
 					].join("\n\n");
 
@@ -222,7 +216,7 @@ function spotifySearch() {
 				}
 				console.log("log.txt was updated!");
 			} else {
-				console.log("Looks like we can't find the song", searchResult)
+				console.log("We can't find the song you're looking for", searchResult);
 			}
 		});
 	}
@@ -230,17 +224,16 @@ function spotifySearch() {
 
 function movieSearch() {
 	for (var i = 3; i < allArgs.length; i++) {
-		searchResult += (allArgs[i] + "+")
+		searchResult += (allArgs[i] + "+");
 	}
 
-	searchResult = searchResult.slice(0, -1)
-
+	searchResult = searchResult.slice(0, -1);
 
 	var queryUrl = "http://www.omdbapi.com/?t=" + searchResult + "&y=&plot=short&apikey=trilogy";
-	var nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy"
+	var nobodyUrl = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
 
 	if (searchResult === "") {
-		omdbResults(nobodyUrl)
+		omdbResults(nobodyUrl);
 	} else {
 		omdbResults(queryUrl);
 	}
@@ -256,8 +249,8 @@ function doWhatItSays() {
 		var dataArr = data.split(",");
 		console.log(dataArr);
 
-		action = dataArr[0]
-		searchResult = dataArr[1]
+		action = dataArr[0];
+		searchResult = dataArr[1];
 
 		switch (action) {
 			case "movie-this":
